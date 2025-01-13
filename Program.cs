f70df4bd88290f3fb73bdbe706e4948f01415361
farhanakhanswe <farhanakhanswe@gmail.com>
2025-01-13 09:03:33 +0600

    two ways to output values

--- Program.cs
+++ Program.cs
@@ -6,12 +6,18 @@ namespace Practice
     {
         static void Main(string[] args)
         {
-            printText();
+            printTextWithConsoleWriteOnly();
         }
 
-        static void printText()
+        static void printTextWithConsoleWriteLine()
         {
             Console.WriteLine("Printing some Text!");
+        }
+
+        static void printTextWithConsoleWriteOnly()
+        {
+            Console.Write("Write will not insert a new line at the end of the output like Write Line");
+            Console.Write("**");
         }
     }
 
